4be159da25f507bdbd59f27b80ebb2675923a0da
Igor Bukur <tuturox91@gmail.com>
2022-11-25 11:28:52 +0200

    Litle fixes

--- src/main/java/taxi/service/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/AuthenticationServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
-    private DriverDao driverDao;
+    private DriverService driverDao;
 
     @Override
     public Driver login(String login, String password) throws AuthenticationException {
